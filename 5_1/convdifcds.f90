64c6f97a8731fc994c2074257f7ad8e5f5de20b4
jtoumey <jtoumey@gmail.com>
2015-02-01 23:02:34 -0600

    setup system parameters and left boundary

--- 5_1/convdifcds.f90
+++ 5_1/convdifcds.f90
@@ -20,20 +20,45 @@ IMPLICIT NONE
 !
 integer n,ii,
 parameter (n=5)
+real dx,L,xmax,x(n)
+real rho,u,gamma
+real phiA,phiB
+!
+!
+!
+L = 1.
+rho = 1.
+u = 0.1
+gamma = 0.1
+phiA = 1.
+phiB = 0.
 
+
+D = gamma/dx
+F = rho*u
+
+
+dx = L/float(n)
+do ii = 1,n
+   x(ii) = (ii-0.5)*dx
+end do
 !
 !...Left boundary
 !
-aw = 0.
-ae
-Su
-Sp
-ap = ae + aw - Sp
+aw =  0.
+ae =  D - F/2.
+Su =  (2.*D + F)*phiA
+Sp = -(2.*D + F)
+ap =  ae + aw - Sp
 !
 a(1) = -aw
 b(1) =  ap
 c(1) = -ae
-d(1) = 
+d(1) =  Su
+!
+do ii = 2,n-1
 
+end do
+!...Right boundary
 
 END
