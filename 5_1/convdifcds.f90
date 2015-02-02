52850dc74ff5e5b4de2dda61cc1c7f0467b56f8a
jtoumey <jtoumey@gmail.com>
2015-02-01 19:45:57 -0600

    added the script for ex. 5.1

---
+++ 5_1/convdifcds.f90
@@ -0,0 +1,27 @@
+!**************************************************************************!
+!                                                                          !
+!  Module:       CONVDIFCDS.F90                                            !
+!                                                                          !
+!  Programmer:   Julian M. Toumey                                          !
+!                Madison, WI                                               !
+!                                                                          !
+!  Date:         February 2015                                             !
+!                                                                          !
+!  Language:     FORTRAN90                                                 !
+!                                                                          !
+!  Description:  This code solves source-free convection-diffusion of a    !
+!                property "phi" in a velocity field "u". The method        !
+!                follows example 5.1 in Versteeg and Malalasekera, 2nd ed. !
+!                The boundary conditions are phi_0 = 1 and phi_L = 0.      !
+!                                                                          !
+!**************************************************************************!
+PROGRAM CONVDIFCDS
+IMPLICIT NONE
+!
+integer n,ii,
+parameter (n=5)
+
+
+
+
+END
