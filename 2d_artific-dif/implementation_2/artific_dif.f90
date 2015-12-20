add30fa44250fd9bf32c9c32d73265d2df86e3b4
Julian Toumey <jtoumey@gmail.com>
2015-12-20 12:24:03 -0600

    artific_dif: added setup for a uniform grid

--- 2d_artific-dif/implementation_2/artific_dif.f90
+++ 2d_artific-dif/implementation_2/artific_dif.f90
@@ -24,9 +24,28 @@ IMPLICIT NONE
 !
 integer nx,ny,ii,jj,kk,iter
 real dx,dy,xmax,ymax
-double precision, dimension(:), allocatable :: x,y
+real, dimension(:), allocatable :: x,y
 !
-write(*,*)'make'
 call read_input(xmax,ymax,nx,ny)
 write(*,*)xmax,ymax,nx,ny
+!
+!   allocate memory for the coordinates of cell centers
+!
+allocate(x(nx),y(ny))
+!
+! setup grid
+!
+dx = xmax/float(nx)
+do ii = 1,nx
+   x(ii) = (ii - 0.5)*dx
+end do
+!
+dy = ymax/float(ny)
+do jj = 1,ny
+   y(jj) = (jj - 0.5)*dy
+end do
+
+!
+deallocate(x,y)
+!
 END
