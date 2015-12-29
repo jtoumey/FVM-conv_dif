bc55c81ce07243238a6167709977f5ac606d7de6
Julian Toumey <jtoumey@gmail.com>
2015-12-29 13:10:12 -0500

    update_implicit: added a check to account for east-most N-S line; previous explicit code treated the aE explicitly, but there was no solution (phi) by which to multiply the coefficient. Maybe the East boundary condition for phi, but I don't think information can travel in that direction.

--- 2d_artific-dif/implementation_2/update_implicit.f90
+++ 2d_artific-dif/implementation_2/update_implicit.f90
@@ -8,12 +8,21 @@ real, dimension(:), intent(inout) :: aw(np),ae(np),Su(np),phi_prev(np)
 !
 !   variables used only in this subroutine
 integer ii
+integer check_index
 !
 !...calculate neighbor coefficients using upwind scheme
 !
-do ii = ny,np-ny
-   write(*,301)Su(ii),aw(ii+1-ny),phi_prev(ii+1-ny),ae(ii+ny),phi_prev(ii+ny)
-   Su(ii) = Su(ii) + aw(ii)*phi_prev(ii+1-ny) + ae(ii)*phi_prev(ii+ny)
+do ii = ny,np
+!   write(*,301)Su(ii),aw(ii+1-ny),phi_prev(ii+1-ny),ae(ii+ny),phi_prev(ii+ny)
+   ! this is the index of the South East cell 
+   ! in the SE cell, there is no solution to the East to treat explicitly
+   check_index = np - ny + 1
+   !
+   if (ii < check_index) then
+      Su(ii) = Su(ii) + aw(ii)*phi_prev(ii+1-ny) + ae(ii)*phi_prev(ii+ny)
+   else
+      Su(ii) = Su(ii) + aw(ii)*phi_prev(ii+1-ny) 
+   end if
 end do
 !
 301 format(3x,f7.2,3x,f7.2,3x,f7.2,3x,f7.2,3x,f7.2,3x)
