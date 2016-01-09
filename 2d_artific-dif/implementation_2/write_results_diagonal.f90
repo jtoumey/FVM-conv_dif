190c779c3061b3e6cb7104d1a80bfebc3dd91141
Julian Toumey <jtoumey@gmail.com>
2016-01-09 15:12:44 -0600

    added a subroutine for writing results on the diagonal

---
+++ 2d_artific-dif/implementation_2/write_results_diagonal.f90
@@ -0,0 +1,16 @@
+SUBROUTINE WRITE_RESULTS_DIAGONAL()
+!
+implicit none
+!
+! variables passed in
+
+!
+! variables used only in this subroutine
+
+!
+
+open(unit=7,file='phi_oblique.dat',ACTION="write", STATUS="replace")
+
+close(12)
+!
+END SUBROUTINE WRITE_RESULTS_DIAGONAL
