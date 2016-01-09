806a323bb641a77f3309903fda14e1ddb4740b34
Julian Toumey <jtoumey@gmail.com>
2016-01-09 16:42:32 -0600

    added code to write exact solution

--- 2d_artific-dif/implementation_2/write_results_diagonal.f90
+++ 2d_artific-dif/implementation_2/write_results_diagonal.f90
@@ -9,17 +9,25 @@ real, dimension(ny), intent(inout) :: y
 !
 ! variables used only in this subroutine
 integer :: diag_index,ii
+real phi_exact
 !
 open(unit=8,file='phi_diagonal.dat',ACTION="write", STATUS="replace")
 !
 do ii = 1,nx
+   !
+   if (ii <= nx/2) then
+      phi_exact = 100.
+   else 
+      phi_exact = 0.
+   end if
+   !
    diag_index = ii*ny - (ii-1)
-   write(8,201)y(ii),phi(diag_index)
+   write(8,201)y(ii),phi(diag_index),phi_exact
 end do
 !
 !   Close the output file
 close(8)
 !
-201 format(3x,f7.2,3x,f7.2)
+201 format(3x,f7.2,3x,f7.2,3x,f7.2)
 !
 END SUBROUTINE WRITE_RESULTS_DIAGONAL
